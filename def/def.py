42c1276785a8c7ddc5305c9944c64021ec40e061
Opilane TTHK <opilane@tthk.ee>
2021-12-13 10:50:57 +0200

    Add project files.

---
+++ def/def.py
@@ -0,0 +1,19 @@
+#from module1 import *
+
+
+#a=20.5
+#b=10.75
+#s=Pindala(float(input("a - ")),float(input("b - ")))
+#print(s)
+
+
+#from module1 import *
+#print(arithmetic(100,200,"+"))
+
+
+#from module1 import *
+#print(is_year_leap(2024))
+
+
+from module1 import *
+print(season(5))
---
+++ def/module1.py
@@ -0,0 +1,58 @@
+#def Pindala(külg1:float,külg2:float)->float:
+#    """Riistküliku pindala leidmine
+#    :param float külg1:Riistküliku(прямоу-ник) esimene(первая) külg(сторона)
+#    :param float külg2:Riistküliku(прямоу-ник) teine(вторая) külg(сторона)
+#    :rtype:float
+#    """
+
+
+#    s=külg1*külg2
+#    return s
+
+#def arithmetic(x:float,y:float,z:str)->float:
+#    """deistvija +,-,*,/
+#    :param float x: pervoe chislo
+#    :param float y: vtoroe chislo
+#    :param float z: deistvie
+#    :rtype:var
+#    """
+#    if z=="+":
+#        otvet=x+y
+#    elif z=="-":
+#        otvet=x-y
+#    elif z=="*":
+#        otvet=x*y
+#    elif z=="/":
+#        otvet=x/y
+#    else:
+#        otvet="Неверная операция"
+#    return otvet
+
+
+
+#def is_year_leap(year:int):
+#    if year % 400 == 0:
+#        return True
+#    if year % 4 == 0 and year % 100 != 0:
+#        return True
+#    return False
+
+#import math
+#def square(a:int):
+#    p=4*a
+#    s=a**2
+#    diagonal=round(a*math.sqrt(2))
+#    return p,s,diagonal
+
+
+#def season(m:int):
+#    if m in (12,1,2):
+#        return "Зима"
+#    elif m in (3,4,5):
+#        return "Весна"
+#    elif m in (6,7,8):
+#        return "Лето"
+#    elif m in (9,10,11):
+#        return "Осень"
+
+
